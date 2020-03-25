4d1f45ed4ffb018939337886c271de6c63b6a09b
Ana Paola Fernandez Sanchez <paola.fernandez@alumnos.udg.mx>
2020-03-24 22:16:07 -0600

    angular

---
+++ Nueva carpeta/src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Dr Nice', points: 10 },
+  { id: 12, name: 'Narco', points: 20 },
+  { id: 13, name: 'Bombasto', points: 30 },
+  { id: 14, name: 'Celeritas', points: 40 },
+  { id: 15, name: 'Magneta', points: 50 },
+  { id: 16, name: 'RubberMan', points: 60 },
+  { id: 17, name: 'Dynama', points: 70 },
+  { id: 18, name: 'Dr IQ', points: 80 },
+  { id: 19, name: 'Magma', points: 90 },
+  { id: 20, name: 'Tornado', points: 100 }
+];
---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Dr Nice', points: 100 },
+  { id: 12, name: 'Narco', points: 90 },
+  { id: 13, name: 'Bombasto', points: 80 },
+  { id: 14, name: 'Celeritas', points: 70 },
+  { id: 15, name: 'Magneta', points: 60 },
+  { id: 16, name: 'RubberMan', points: 50 },
+  { id: 17, name: 'Dynama', points: 40 },
+  { id: 18, name: 'Dr IQ', points: 30 },
+  { id: 19, name: 'Magma', points: 20 },
+  { id: 20, name: 'Tornado', points: 10 }
+];
